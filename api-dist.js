b88f0b81ff8dc620d7f825531038d4e3dd75e2fc
Yuhuai Liu <adlius45@hotmail.com>
2019-03-12 02:24:07 -0400

    Add changelog. Modify README. Modify api-dist.js with configuration instructions.

--- api-dist.js
+++ api-dist.js
@@ -1,5 +1,8 @@
 const Devour = require('devour-client');
 
+
+// To configure the external API from which the chatbot fetch keyworded responses
+// Change `apiaddress` to your external api address.
 const jsonApi = new Devour(
     {
         apiUrl: 'apiaddress'
@@ -7,6 +10,8 @@ const jsonApi = new Devour(
 )
 
 // Define Model
+// If your KeywordedResponse list view is in a different path
+// Change `collectionPath` to the corresponding path for the resource.
 jsonApi.define('keyworded_response', {
     keyword: '',
     response: '',
